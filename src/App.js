b9c410c87b9d18c2e4f4620ff3d9789a51208679
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-03 12:28:45 +0330

    App component completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
+import ExpenseApp from "./components/ExpenseApp";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import Transactions from "./components/Transactions/Transactions";
 const App = () => {
-  return <h1>ExpenseTracker</h1>;
+  return (
+    <div className="App container">
+      <h1>محاسبه گر دخل و خرج</h1>
+      <ExpenseApp>
+        <ExpenseInfo />
+        <Transactions />
+      </ExpenseApp>
+    </div>
+  );
 };
 
 export default App;
