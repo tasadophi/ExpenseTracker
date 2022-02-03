b25553943f109ed97b86c9f6ba7d1735da79be16
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-03 12:13:32 +0330

    add id for transactions

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -3,10 +3,20 @@ import React, { useContext, useReducer } from "react";
 const transactionsContext = React.createContext();
 const transactionsContextDispatcher = React.createContext();
 
+const ids = [];
+
+const generateId = () => {
+  let id = Math.floor(Math.random() * 1000);
+  while (ids.includes(id)) {
+    id = Math.floor(Math.random() * 1000);
+  }
+  return id;
+};
+
 const reducer = (state, action) => {
   const type = action.type;
   if (type === "add") {
-    return [...state, action.transaction];
+    return [...state, { ...action.transaction, id: generateId() }];
   }
 };
 
