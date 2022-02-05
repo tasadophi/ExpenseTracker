0845983c737acdf227948d7faedd3f3679dc32fc
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-05 16:07:42 +0330

    edit functions added

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,32 +1,47 @@
 import { useState } from "react";
-import { useTransactionsDispatcher } from "../ExpenseApp";
+import { useTransactions, useTransactionsDispatcher } from "../ExpenseApp";
 import styles from "./ExpenseForm.module.css";
 
-const ExpenseForm = () => {
-  const [transaction, setTransaction] = useState({
-    type: "income",
-    amount: "",
-    desc: "",
-  });
+const ExpenseForm = ({ editId }) => {
+  const transactions = useTransactions();
+  const transactionObject = editId
+    ? transactions.find((t) => t.id === parseInt(editId))
+    : {
+        type: "income",
+        amount: 0,
+        desc: "",
+      };
+  const [transaction, setTransaction] = useState(transactionObject);
   const dispatch = useTransactionsDispatcher();
+  const descPlaceholder = editId ? "توضیحات جدید..." : "توضیحات تراکنش...";
+  const amountPlaceholder = editId
+    ? "مبلغ جدید به تومان"
+    : "مبلغ تراکنش به تومان";
+  const btnText = editId ? "ویرایش" : "افزودن";
+
   const setTransactionInfo = (e, property) =>
     setTransaction((prev) => {
       return { ...prev, [property]: e.target.value };
     });
 
-  const submitHandler = (e) => {
+  const addHandler = (e) => {
     e.preventDefault();
     dispatch({ type: "add", transaction: transaction });
   };
 
+  const editHandler = (e) => {
+    e.preventDefault();
+    dispatch({ type: "edit", transaction: transaction });
+  };
+
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={editId ? editHandler : addHandler}>
       <input
         onChange={(e) => setTransactionInfo(e, "desc")}
         value={transaction.desc}
         className={styles.inputs}
         type="text"
-        placeholder="توضیحات تراکنش..."
+        placeholder={descPlaceholder}
       ></input>
       <input
         onChange={(e) => setTransactionInfo(e, "amount")}
@@ -34,7 +49,7 @@ const ExpenseForm = () => {
         className={styles.inputs}
         name="amount"
         type="number"
-        placeholder="مبلغ تراکنش به تومان"
+        placeholder={amountPlaceholder}
       ></input>
       <div className={styles.radios}>
         <div className={styles.radio}>
@@ -60,7 +75,7 @@ const ExpenseForm = () => {
           ></input>
         </div>
       </div>
-      <button className={`${styles.btn}`}>افزودن</button>
+      <button className={`${styles.btn}`}>{btnText}</button>
     </form>
   );
 };
