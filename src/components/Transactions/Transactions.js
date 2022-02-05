44e464a6a9d1e39378b87e7c67d856ae5894bf0e
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2022-02-05 17:44:56 +0330

    search transactions added

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,16 +1,44 @@
+import { useEffect, useRef, useState } from "react";
 import { useTransactions } from "../ExpenseApp";
 import Transaction from "../Transaction/Transaction";
 import styles from "./Transactions.module.css";
 const Transactions = () => {
   const transactions = useTransactions();
+  const [input, setInput] = useState("");
+  const [transactionsCopy, setTransactionsCopy] = useState([]);
+  useEffect(() => {
+    setTransactionsCopy(transactions);
+    setTransactionsCopy(
+      transactions.filter((t) =>
+        t.desc.toLowerCase().includes(input.toLowerCase())
+      )
+    );
+  }, [transactions, input]);
+  
+  const filterTransactions = (e) => {
+    setInput(e.target.value);
+    setTransactionsCopy(
+      transactions.filter((t) =>
+        t.desc.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  };
   return (
     <section className={styles.transactions}>
       {transactions.length ? (
-        <h2>تراکنش های شما</h2>
+        <>
+          <h2>تراکنش های شما</h2>
+          <input
+            value={input}
+            onChange={filterTransactions}
+            type="text"
+            placeholder="جستجو کنید..."
+          ></input>
+        </>
       ) : (
         <h2>شما هیچ تراکنشی ندارید!</h2>
       )}
-      {transactions.map((t) => {
+      {transactionsCopy.map((t) => {
         return t.type === "income" ? (
           <Transaction
             key={t.id}
